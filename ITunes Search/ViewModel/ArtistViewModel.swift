4ac7ca942b7ee4c8118116d816ff62dbc38f9ef0
Nicolas Rios <nickrios33@yahoo.com>
2021-05-11 13:58:37 -0700

    Update ArtistViewModel.swift

--- ITunes Search/ViewModel/ArtistViewModel.swift
+++ ITunes Search/ViewModel/ArtistViewModel.swift
@@ -38,12 +38,12 @@ struct ArtistViewModel {
     }
     
     var Date: String {
-        //1. Convert String date back to Date format
+        
         if let convertedDate = Self.dateFormatterToDate.date(from: artist.releaseDate) {
-        //2. If happens format nicer and give me back as String
+       
             return Self.dateFormatterToString.string(from: convertedDate)
         }
-        //3. If something goes wrong still I don't want to crash, so give me back the originall
+       
          return artist.releaseDate
     }
 }
